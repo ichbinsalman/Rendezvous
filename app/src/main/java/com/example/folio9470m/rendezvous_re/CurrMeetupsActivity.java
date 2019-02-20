56896a15fe6105d7b9bc2e12ea19502fd5cfde90
Salman Zaidi <saz.zdi@gmail.com>
2019-02-20 17:03:57 +0100

    add app folder

---
+++ app/src/main/java/com/example/folio9470m/rendezvous_re/CurrMeetupsActivity.java
@@ -0,0 +1,13 @@
+package com.example.folio9470m.rendezvous_re;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrMeetupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curr_meetups);
+    }
+}
